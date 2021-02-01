a0fc2333b05963feaeeed7919167fde7d8060a31
tokku5552 <physics.0121@gmail.com>
2021-02-02 01:12:02 +0900

    initial commit

---
+++ src/Blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Blog extends React.Component{
+    constructor(props){
+        super(props);   
+    }
+    render(){
+        return(
+            <div>
+                <h2>おはよう</h2>
+            </div>
+
+        )
+    }
+}
+export default Blog
