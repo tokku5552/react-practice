d49ec2bd72f85ee40613b0165cc2b550bad85f73
tokku5552 <physics.0121@gmail.com>
2021-02-03 00:18:31 +0900

    add Article.jsx

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import Article from './Article';
 
-class Blog extends React.Component{
-    constructor(props){
-        super(props);   
+class Blog extends React.Component {
+    constructor(props) {
+        super(props);
     }
-    render(){
-        return(
-            <div>
-                <h2>おはよう</h2>
-            </div>
+    render() {
+        return (
+            <>
+                <Article title={'Reactの使い方'}/>
+                <Article />
+            </>
 
         )
     }
 }
+
 export default Blog
