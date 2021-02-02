8ff2b559763ff7f4495ca86a6ea2e5d0d4ee6bbb
tokku5552 <physics.0121@gmail.com>
2021-02-03 01:19:29 +0900

    #5まで完了

--- src/Article.jsx
+++ src/Article.jsx
@@ -4,6 +4,7 @@ const Article = (props) => {
     return (
         <div>
             <h2>{props.title}です</h2>
+            <p>公開設定：<input type='checkbox' checked={props.isPublished} id="chekc" onClick={()=>props.toggle()}/></p>
         </div>
     )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,12 +4,20 @@ import Article from './Article';
 class Blog extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isPublished: false,
+        }
     }
+
+    toggledPublish = () => {
+        this.setState({
+            isPublished: !this.state.isPublished
+        })
+    };
     render() {
         return (
             <>
-                <Article title={'Reactの使い方'}/>
-                <Article />
+                <Article title={'Reactの使い方'} isPublished={this.state.isPublished} toggle={()=>this.toggledPublish()}/>
             </>
 
         )
